929c993ea06f0d06228ef47936566743bda8d2e7
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-07-05 11:16:59 +0100

    joy

--- test_database.py
+++ test_database.py
@@ -1,6 +1,7 @@
+import pytest
 from unittest.mock import MagicMock
 
-from database import get_all_messages, get_all_messages_no_with
+from database import get_all_messages, get_all_messages_no_with, insert_message
 
 
 def test_get_all_messages_returns_list():
@@ -29,3 +30,36 @@ def test_get_all_messages_no_with_returns_list():
     assert fake_fetch.call_count == 1
     assert fake_execute.call_count == 1
     assert fake_execute.call_args[0] == ("SELECT * FROM message;",)
+
+
+"""test insert returns tuple
+test error raised if input not str
+test .execute() called
+test correct sql executed
+"""
+
+
+def test_insert_returns_tuple():
+
+    fake_connection = MagicMock()
+
+    fake_connection.cursor().__enter__().fetchone.return_value = 123, "test"
+
+    result = insert_message(fake_connection, "Hello there!")
+
+    assert isinstance(result, tuple)
+
+
+def test_insert_raises_error():
+
+    fake_connection = MagicMock
+    with pytest.raises(TypeError) as e_info:
+        insert_message(fake_connection, 12)
+
+
+def test_insert_called_once():
+
+    fake_connection = MagicMock()
+    insert_message(fake_connection, "hi")
+    assert fake_connection.cursor().__enter__.execute().call_count == 1
+    # incomplete
